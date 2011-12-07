90b1d3a0e93972b0e8754ca1103b25a8cebdfd7e
Brendan Nee <brendan.nee@gmail.com>
2011-12-07 02:18:32 -0800

    Work on downloading and reading GTFS

--- routes.js
+++ routes.js
@@ -1,22 +1,38 @@
-var errors = require('./lib/util/errors');
+var errors = require('./lib/util/errors')
+  , api = require('./lib/util/api');
 
 module.exports = function routes(app){
 
-  app.get('/api', function(req, res){
+  //Routelist
+  app.get('/api/routes/:agency', function(req, res){
 
     //Spit back variables from URL
     for(i in req.query){ console.log(i+": "+req.query[i]); }
   
     res.contentType('application/json');
-
     res.send({
-      error: 'none'
+        agency: req.params.agency
+      , routes: [
+        {
+          shortName: '35',
+          routeName: '35-Eureka',
+          predictionType: 'schedule'
+        }
+      ]
     });
       
   });
-
-  app.all('*', function notFound(req, res, next) {
-    next(new errors.NotFound);
+  
+  
+  app.get('/api/routes/download/:agency', api.downloadGTFS);
+  
+  //Nothing specified
+  app.all('*', function notFound(req, res) {
+    
+    res.contentType('application/json');
+    res.send({
+      error: 'No API call specified'
+    });
   });
 
 }
