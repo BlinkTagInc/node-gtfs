84746b139068c6b93cf1020383550541ec9e68e7
Brendan Nee <brendan.nee@gmail.com>
2012-02-13 18:00:09 -0500

    Endpoints to get all agencies and get all routes for a specified agency

--- routes.js
+++ routes.js
@@ -3,28 +3,14 @@ var errors = require('./lib/util/errors')
 
 module.exports = function routes(app){
 
-  //Routelist
-  app.get('/api/routes/:agency', function(req, res){
+  //AgencyList
+   app.get('/api/agencies', api.getAllAgencies);
 
-    //Spit back variables from URL
-    for(i in req.query){ console.log(i+": "+req.query[i]); }
-  
-    res.contentType('application/json');
-    res.send({
-        agency: req.params.agency
-      , routes: [
-        {
-          shortName: '35',
-          routeName: '35-Eureka',
-          predictionType: 'schedule'
-        }
-      ]
-    });
-      
-  });
+  //Routelist
+  app.get('/api/routes/:agency', api.getRoutesByAgency);
   
   
-  app.get('/api/routes/download/:agency', api.downloadGTFS);
+  app.get('/api/download/:agency', api.downloadGTFS);
   
   //Nothing specified
   app.all('*', function notFound(req, res) {
