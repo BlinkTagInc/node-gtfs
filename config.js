723c6ea665a923e1bbd71ea26c3665efa07fdcb0
Brendan Nee <brendan.nee@gmail.com>
2012-03-05 19:09:20 -0500

    Added SF bay area agencies to config

--- config.js
+++ config.js
@@ -2,6 +2,13 @@ module.exports = {
     mongo_node_host: 'localhost'
   , mongo_node_database: 'db'
   , agencies: [
-      'capital-metro'
+        'caltrain'
+      , 'airbart'
+      , 'alamedaoakland-ferry'
+      , 'ac-transit'
+      , 'county-connection'
+      , 'san-francisco-municipal-transportation-agency'
+      , 'bay-area-rapid-transit'
+      , 'golden-gate-ferry'
     ]
 }
