270639c3dbbf9f70e5b28d02fd944f9fa9848ed4
Brendan Nee <brendan.nee@gmail.com>
2012-08-13 15:17:54 -0700

    Updated to work with express 3.x

--- examples/express/config.js
+++ examples/express/config.js
@@ -12,7 +12,7 @@ module.exports = function(app){
 
   // Dev
   app.configure('development', function(){
-    this.use(express.profiler())
+    this
       .use(express.logger('\x1b[90m:remote-addr -\x1b[0m \x1b[33m:method\x1b[0m' +
          '\x1b[32m:url\x1b[0m :status \x1b[90m:response-time ms\x1b[0m'))
       .use(express.errorHandler({dumpExceptions: true, showStack: true}))
