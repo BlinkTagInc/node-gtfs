0fd58b0f8bf5a6badd9f70a7aab6249d85e99110
Brendan Nee <brendan@blinktag.com>
2014-12-27 18:03:36 -0800

    Better config.js

--- config.js
+++ config.js
@@ -4,16 +4,18 @@ if (process.env.NODE_ENV === 'test'){
 }
 
 module.exports = {
-  mongo_url: process.env.MONGOHQ_URL || 'mongodb://localhost:27017/gtfs'
-  , agencies: [
-    /* Put agency_key names from gtfs-data-exchange.com.
-     Optionally, specify a download URL to use a dataset not from gtfs-data-exchange.com */
-    'alamedaoakland-ferry'
-    , { agency_key: 'caltrain', url: 'http://www.gtfs-data-exchange.com/agency/caltrain/latest.zip'}
-    , 'ac-transit'
-    , 'county-connection'
-    , 'san-francisco-municipal-transportation-agency'
-    , 'bay-area-rapid-transit'
-    , 'golden-gate-ferry'
+  mongo_url: process.env.MONGOHQ_URL || 'mongodb://localhost:27017/gtfs',
+  agencies: [
+    /*
+      Put agency_key names from gtfs-data-exchange.com.
+      Optionally, specify a download URL to use a dataset not from gtfs-data-exchange.com
+    */
+
+    { agency_key: 'caltrain', url: 'http://www.gtfs-data-exchange.com/agency/caltrain/latest.zip' },
+    'ac-transit',
+    'county-connection',
+    'san-francisco-municipal-transportation-agency',
+    'bay-area-rapid-transit',
+    'golden-gate-ferry'
   ]
 };
