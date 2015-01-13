f6d002f212da3677723291561aa3c3b4f9153cc4
Brendan Nee <brendan@blinktag.com>
2015-01-13 02:20:02 -0800

    Better routes

--- examples/express/routes/index.js
+++ examples/express/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var gtfs = require('gtfs');
 
-router.get('/', function(req, res) {
+router.get('/', function(req, res, next) {
   res.render('index', { title: 'Node GTFS Demo' });
 });
 
