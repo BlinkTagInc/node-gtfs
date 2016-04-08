8bf6f8d1b0cafdb7aa68500d881a0a4e76c4fbce
David Aghassi <david.aghassi@gmail.com>
2016-04-08 00:34:44 -0400

    Added ensureAsync, fixed memory overflow
    -  Read through async documentation, followed steps
    they recommend to avoid memory overflow
    -  Read through csv-parser to see how it worked,
    came up with a solution to memory overflow issue.
     This solution worked on my 90mb data set from NJ
     Transit. This may not solve large scale issues...

--- scripts/download.js
+++ scripts/download.js
@@ -87,8 +87,8 @@ function main(config, callback) {
       async.series([
         cleanupFiles,
         getFiles,
-        removeDatabase,
-        importFiles,
+        async.ensureAsync(removeDatabase),
+        async.ensureAsync(importFiles),
         postProcess,
         cleanupFiles
       ], function(e) {
@@ -184,10 +184,12 @@ function main(config, callback) {
         }
 
         async.forEach(filenames, function(filename, cb) {
-          db.collection(filename.collection, function(e, collection) {
-            collection.remove({
-              agency_key: agency_key
-            }, cb);
+          async.setImmediate(function () {
+           db.collection(filename.collection, function(e, collection) {
+             collection.remove({
+               agency_key: agency_key
+             }, cb);
+           });
           });
         }, function(e) {
           cb(e, 'remove');
@@ -198,151 +200,152 @@ function main(config, callback) {
       function importFiles(cb) {
         // Loop through each file and add agency_key
         async.forEachSeries(filenames, function(filename, cb) {
-          var filepath = path.join(gtfsDir, filename.fileNameBase + '.txt');
+          async.setImmediate(function () {
+            var filepath = path.join(gtfsDir, filename.fileNameBase + '.txt');
 
-          if (!fs.existsSync(filepath)) {
-            if (!filename.nonstandard) {
-              log(agency_key + ': Importing data - No ' + filename.fileNameBase + '.txt file found');
+            if (!fs.existsSync(filepath)) {
+              if (!filename.nonstandard) {
+                log(agency_key + ': Importing data - No ' + filename.fileNameBase + '.txt file found');
+              }
+
+              return cb();
             }
 
-            return cb();
-          }
+            log(agency_key + ': Importing data - ' + filename.fileNameBase + '.txt');
+            db.collection(filename.collection, function(e, collection) {
+              if (e) return handleError(e);
+              var input = fs.createReadStream(filepath);
+              var parseOn = 'data';
 
-          log(agency_key + ': Importing data - ' + filename.fileNameBase + '.txt');
-          db.collection(filename.collection, function(e, collection) {
-            if (e) return handleError(e);
-            var input = fs.createReadStream(filepath);
+              var parser = csv.parse({
+                columns: true,
+                relax: true,
+                skip_empty_lines: true
+              });
 
-            var parser = csv.parse({
-              columns: true,
-              relax: true
-            });
+              if (parser.lines > 1000) {
+                parseOn = 'readable'
+              }
+              parser.on(parseOn, function() {
+                while(line = parser.read()) {
 
-            parser.on('readable', function() {
-              while(line = parser.read()) {
-                //remove null values
-                for(var key in line) {
-                  if (line[key] === null) {
-                    delete line[key];
-                  }
-                }
+                  //add agency_key
+                  line.agency_key = agency_key;
 
-                //add agency_key
-                line.agency_key = agency_key;
-
-                //convert fields that should be int
-                var integerFields = [
-                  'monday',
-                  'tuesday',
-                  'wednesday',
-                  'thursday',
-                  'friday',
-                  'saturday',
-                  'sunday',
-                  'start_date',
-                  'end_date',
-                  'date',
-                  'exception_type',
-                  'shape_pt_sequence',
-                  'payment_method',
-                  'transfers',
-                  'transfer_duration',
-                  'feed_start_date',
-                  'feed_end_date',
-                  'headway_secs',
-                  'exact_times',
-                  'route_type',
-                  'direction_id',
-                  'location_type',
-                  'wheelchair_boarding',
-                  'stop_sequence',
-                  'pickup_type',
-                  'drop_off_type',
-                  'use_stop_sequence',
-                  'transfer_type',
-                  'min_transfer_time',
-                  'wheelchair_accessible',
-                  'bikes_allowed',
-                  'timepoint'
-                ];
-
-                integerFields.forEach(function(fieldName) {
-                  if (line[fieldName]) {
-                    line[fieldName] = parseInt(line[fieldName], 10);
-                  }
-                });
-
-                //convert fields that should be float
-                var floatFields = [
-                  'price',
-                  'shape_dist_traveled',
-                  'shape_pt_lat',
-                  'shape_pt_lon',
-                  'stop_lat',
-                  'stop_lon'
-                ];
-
-                floatFields.forEach(function(fieldName) {
-                  if (line[fieldName]) {
-                    line[fieldName] = parseFloat(line[fieldName]);
-                  }
-                });
-
-                // make lat/lon array for stops
-                if (line.stop_lat && line.stop_lon) {
-                  line.loc = [
-                    line.stop_lon,
-                    line.stop_lat
+                  //convert fields that should be int
+                  var integerFields = [
+                    'monday',
+                    'tuesday',
+                    'wednesday',
+                    'thursday',
+                    'friday',
+                    'saturday',
+                    'sunday',
+                    'start_date',
+                    'end_date',
+                    'date',
+                    'exception_type',
+                    'shape_pt_sequence',
+                    'payment_method',
+                    'transfers',
+                    'transfer_duration',
+                    'feed_start_date',
+                    'feed_end_date',
+                    'headway_secs',
+                    'exact_times',
+                    'route_type',
+                    'direction_id',
+                    'location_type',
+                    'wheelchair_boarding',
+                    'stop_sequence',
+                    'pickup_type',
+                    'drop_off_type',
+                    'use_stop_sequence',
+                    'transfer_type',
+                    'min_transfer_time',
+                    'wheelchair_accessible',
+                    'bikes_allowed',
+                    'timepoint'
                   ];
 
-                  // if coordinates are not specified, use [0,0]
-                  if (isNaN(line.loc[0])) {
-                    line.loc[0] = 0;
-                  }
-                  if (isNaN(line.loc[1])) {
-                    line.loc[1] = 0;
+                  integerFields.forEach(function(fieldName) {
+                    if (line[fieldName]) {
+                      line[fieldName] = parseInt(line[fieldName], 10);
+                    }
+                  });
+
+                  //convert fields that should be float
+                  var floatFields = [
+                    'price',
+                    'shape_dist_traveled',
+                    'shape_pt_lat',
+                    'shape_pt_lon',
+                    'stop_lat',
+                    'stop_lon'
+                  ];
+
+                  floatFields.forEach(function(fieldName) {
+                    if (line[fieldName]) {
+                      line[fieldName] = parseFloat(line[fieldName]);
+                    }
+                  });
+
+                  // make lat/lon array for stops
+                  if (line.stop_lat && line.stop_lon) {
+                    line.loc = [
+                      line.stop_lon,
+                      line.stop_lat
+                    ];
+
+                    // if coordinates are not specified, use [0,0]
+                    if (isNaN(line.loc[0])) {
+                      line.loc[0] = 0;
+                    }
+                    if (isNaN(line.loc[1])) {
+                      line.loc[1] = 0;
+                    }
+
+                    // Convert to epsg4326 if needed
+                    if (task.agency_proj) {
+                      line.loc = proj4(task.agency_proj, 'WGS84', line.loc);
+                      line.stop_lon = line.loc[0];
+                      line.stop_lat = line.loc[1];
+                    }
+
+                    // Calulate agency bounds
+                    if (agency_bounds.sw[0] > line.loc[0] || !agency_bounds.sw[0]) {
+                      agency_bounds.sw[0] = line.loc[0];
+                    }
+                    if (agency_bounds.ne[0] < line.loc[0] || !agency_bounds.ne[0]) {
+                      agency_bounds.ne[0] = line.loc[0];
+                    }
+                    if (agency_bounds.sw[1] > line.loc[1] || !agency_bounds.sw[1]) {
+                      agency_bounds.sw[1] = line.loc[1];
+                    }
+                    if (agency_bounds.ne[1] < line.loc[1] || !agency_bounds.ne[1]) {
+                      agency_bounds.ne[1] = line.loc[1];
+                    }
                   }
 
-                  // Convert to epsg4326 if needed
-                  if (task.agency_proj) {
-                    line.loc = proj4(task.agency_proj, 'WGS84', line.loc);
-                    line.stop_lon = line.loc[0];
-                    line.stop_lat = line.loc[1];
+                  //make lat/long for shapes
+                  if (line.shape_pt_lat && line.shape_pt_lon) {
+                    line.loc = [line.shape_pt_lon, line.shape_pt_lat];
                   }
 
-                  // Calulate agency bounds
-                  if (agency_bounds.sw[0] > line.loc[0] || !agency_bounds.sw[0]) {
-                    agency_bounds.sw[0] = line.loc[0];
-                  }
-                  if (agency_bounds.ne[0] < line.loc[0] || !agency_bounds.ne[0]) {
-                    agency_bounds.ne[0] = line.loc[0];
-                  }
-                  if (agency_bounds.sw[1] > line.loc[1] || !agency_bounds.sw[1]) {
-                    agency_bounds.sw[1] = line.loc[1];
-                  }
-                  if (agency_bounds.ne[1] < line.loc[1] || !agency_bounds.ne[1]) {
-                    agency_bounds.ne[1] = line.loc[1];
-                  }
+                  //insert into db
+                  collection.insert(line, function(e) {
+                    if (e) {
+                      handleError(e);
+                    }
+                  });
                 }
-
-                //make lat/long for shapes
-                if (line.shape_pt_lat && line.shape_pt_lon) {
-                  line.loc = [line.shape_pt_lon, line.shape_pt_lat];
-                }
-
-                //insert into db
-                collection.insert(line, function(e) {
-                  if (e) {
-                    handleError(e);
-                  }
-                });
-              }
+              });
+              parser.on('end', function() {
+                cb();
+              });
+              parser.on('error', handleError);
+              input.pipe(parser);
             });
-            parser.on('end', function() {
-              cb();
-            });
-            parser.on('error', handleError);
-            input.pipe(parser);
           });
         }, function(e) {
           cb(e, 'import');
