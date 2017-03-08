759f00cbe57f577b48a1ca10dfdd2f9c2701a746
David Aghassi <david.aghassi@gmail.com>
2017-03-08 14:12:21 -0500

    Updated Stop.js, created tests
    -  Updated Stop.js to index stop_code
    -  Created getStopsByCode.js to run basic tests

---
+++ test/mocha/gtfs.getStopsByCode.js
@@ -0,0 +1,90 @@
+const async = require('async');
+const path = require('path');
+const should = require('should');
+
+// libraries
+const config = require('../config.json');
+const gtfs = require('../../');
+
+
+// test support
+const database = require('../support/database');
+
+// setup fixtures
+const agenciesFixtures = [{
+  agency_key: 'caltrain',
+  path: path.join(__dirname, '../fixture/caltrain_20160406.zip')
+}];
+
+config.agencies = agenciesFixtures;
+
+describe('gtfs.getStopsByStopCode(): ', () => {
+
+  before((done) => {
+    database.connect(config, done);
+  });
+
+  after((done) => {
+    async.series({
+      teardownDatabase: (next) => {
+        database.teardown(next);
+      },
+      closeDb: (next) => {
+        database.close(next);
+      }
+    }, done);
+  });
+
+  beforeEach((done) => {
+    async.series({
+      teardownDatabase: (next) => {
+        database.teardown(next);
+      },
+      executeDownloadScript: (next) => {
+        gtfs.import(config, next);
+      }
+    }, done);
+  });
+
+  it('should return an empty array if no stops exists for given agency', (done) => {
+    async.series({
+      teardownDatabase: (next) => {
+        database.teardown(next);
+      }
+    },() => {
+      const agency_key = 'non_existing_agency';
+      gtfs.getStops(agency_key, (err, stops) => {
+        should.exist(stops);
+        stops.should.have.length(0);
+        done();
+      });
+    });
+  });
+
+  it('should return array of stops if it exists for given agency', (done) => {
+    const agency_key = 'caltrain';
+
+    gtfs.getStopsByStopCode(agency_key, (err, stops) => {
+      should.not.exist(err);
+      should.exist(stops);
+
+      stops.should.have.length(95);
+      done();
+    });
+  });
+
+  it('should return array of stops if it exists for given agency, and stop_ids', (done) => {
+    const agency_key = 'caltrain';
+    const stop_codes = [
+      '70031',
+      '70061'
+    ];
+
+    gtfs.getStopsByStopCode(agency_key, stop_codes, (err, stops) => {
+      should.not.exist(err);
+      should.exist(stops);
+      stops.should.have.length(2);
+      done();
+    });
+  });
+});
