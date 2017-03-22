ccac7d5b21c94db73517602aad599ae22749bf96
Brendan Nee <brendan@blinktag.com>
2017-03-22 11:55:55 +0000

    geoJSON methods for stops

---
+++ test/mocha/gtfs.get-stops-as-geojson.js
@@ -0,0 +1,92 @@
+const path = require('path');
+
+const async = require('async');
+const should = require('should');
+
+// libraries
+const config = require('../config.json');
+const gtfs = require('../../');
+
+// test support
+const database = require('../support/database');
+
+// setup fixtures
+const agenciesFixtures = [{
+  agency_key: 'caltrain',
+  path: path.join(__dirname, '../fixture/caltrain_20160406.zip')
+}];
+
+config.agencies = agenciesFixtures;
+
+describe('gtfs.getStopsAsGeoJSON(): ', () => {
+  before(done => {
+    database.connect(config, done);
+  });
+
+  after(done => {
+    async.series({
+      teardownDatabase: next => {
+        database.teardown(next);
+      },
+      closeDb: next => {
+        database.close(next);
+      }
+    }, done);
+  });
+
+  beforeEach(done => {
+    async.series({
+      teardownDatabase: next => {
+        database.teardown(next);
+      },
+      executeDownloadScript: next => {
+        gtfs.import(config, next);
+      }
+    }, done);
+  });
+
+  it('should return geojson with an empty features array if no stops exist for given agency', done => {
+    async.series({
+      teardownDatabase: next => {
+        database.teardown(next);
+      }
+    }, () => {
+      const agencyKey = 'non_existing_agency';
+      gtfs.getStopsAsGeoJSON(agencyKey, (err, geojson) => {
+        should.not.exist(err);
+        should.exist(geojson);
+        geojson.type.should.equal('FeatureCollection');
+        geojson.features.should.have.length(0);
+        done();
+      });
+    });
+  });
+
+  it('should return geojson with stops if they exist for given agency', done => {
+    const agencyKey = 'caltrain';
+
+    gtfs.getStopsAsGeoJSON(agencyKey, (err, geojson) => {
+      should.not.exist(err);
+      should.exist(geojson);
+      geojson.type.should.equal('FeatureCollection');
+      geojson.features.should.have.length(95);
+      done();
+    });
+  });
+
+  it('should return geojson with stops if they exist for given agency and stopIds', done => {
+    const agencyKey = 'caltrain';
+    const stopIds = [
+      '70031',
+      '70061'
+    ];
+
+    gtfs.getStopsAsGeoJSON(agencyKey, stopIds, (err, geojson) => {
+      should.not.exist(err);
+      should.exist(geojson);
+      geojson.type.should.equal('FeatureCollection');
+      geojson.features.should.have.length(2);
+      done();
+    });
+  });
+});
