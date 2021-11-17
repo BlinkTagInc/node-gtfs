88953c3de2597053d6ebaf804fb5a3c710e7ddd9
Matt Moran <mattmoran2000@gmail.com>
2021-11-17 13:32:09 +1300

    Add basic test for TypeScript types

---
+++ @types/tests.ts
@@ -0,0 +1,24 @@
+import { Config, exportGtfs, getAgencies, getDb, importGtfs, openDb } from "gtfs";
+
+/**
+ * This is type-checked (but not executed) by dtslint.
+ * `npm run dtslint`
+ */
+
+(async () => {
+    const config: Config = {
+        agencies: [{ path: "example.zip" }],
+        verbose: false,
+    };
+
+    await importGtfs(config); // $ExpectType void
+
+    await exportGtfs(config); // $ExpectType void
+
+    await openDb(config); // $ExpectType Database<Database, Statement>
+
+    await getDb(); // $ExpectType Database<Database, Statement>
+
+    const results = await getAgencies();
+    results[0]; // $ExpectType Record<string, any>
+})();
